53e35b425afa449bfc6089623784b930cc3607c2
ADiBariya <adiiibariya@gmail.com>
2025-12-08 23:24:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # Bot Configuration - Main credentials
 API_ID = int(os.environ.get("API_ID", "22733269"))
 API_HASH = os.environ.get("API_HASH", "d1d8331e5b288c572e8bb6baa7d8f833")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7597300015:AAHPdbsCK1X_GeNOX4yO-ITRo3zqPkVXvz0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7890556580:AAFesFBvv20MjoMl4pnGUUyQgnIL7DKXuyw")
 
 # Owner Configuration
 import os
