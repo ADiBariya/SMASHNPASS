46e70d1816a16ea0019e7af3a80a7283bacf57e5
ADiBariya <adiiibariya@gmail.com>
2025-12-14 09:34:58 +0530

    Update user_client.py

--- core/user_client.py
+++ core/user_client.py
@@ -1,10 +1,9 @@
-# core/user_client.py
 from pyrogram import Client
-from config import API_ID, API_HASH, USER_SESSION
+from config import USERBOT_API_ID, USERBOT_API_HASH, USER_SESSION
 
 user = Client(
     name="user_session",
-    api_id=API_ID,
-    api_hash=API_HASH,
+    api_id=USERBOT_API_ID,
+    api_hash=USERBOT_API_HASH,
     session_string=USER_SESSION
 )
